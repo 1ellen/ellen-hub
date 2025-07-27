180e8612e59883d627588449705a45fcec55096f
Ellen <ellennf623@gmail.com>
2025-07-27 14:09:25 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,36 @@
+local ID = game.PlaceId
+local baseURL = "https://github.com/1ellen/ellen-hub/tree/main/"
+local Players = game:GetService("Players")
+local plr = Players.LocalPlayer
 
+function GetGame()
+    if ID == 109983668079237 then
+        return "stealabrainrot.lua"
+    else
+        plr:Kick("Jogo sem suporte")
+        return nil
+    end
+end
+
+local gameScript = GetGame()
+
+if gameScript then
+    loadstring(game:HttpGet(baseURL .. gameScript))()
+end
+
+for _, v in next, getconnections(plr.Idled) do
+    v:Disable()
+end
+
+local VirtualUser = game:GetService("VirtualUser")
+
+if not plr then
+    error("Falha ao obter referência: LocalPlayer")
+end
+
+plr.Idled:Connect(function()
+    pcall(function()
+        VirtualUser:CaptureController()
+        VirtualUser:ClickButton2(Vector2.new())
+    end)
+end)
