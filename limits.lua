7ec4e72caac57d5db750df3259bbe1f0b2e0e859
Ellen <ellennf623@gmail.com>
2025-07-31 21:20:27 -0400

    Update limits.lua

--- limits.lua
+++ limits.lua
@@ -401,20 +401,6 @@ local HighlightToggle = Tab_ESP:Toggle({
 local Baseplate = workspace:WaitForChild("Baseplate")
 local LastPosition
 
-
-local TpBackButton = Tab_Map:Button({
-    Title = "TP Back",
-    Desc = "last cframe: None",
-    Locked = false,
-    Callback = function()
-        local character = game:GetService("Players").LocalPlayer.Character
-        local rootPart = character and character:FindFirstChild("HumanoidRootPart")
-        if rootPart and LastPosition then
-            rootPart.CFrame = LastPosition
-        end
-    end
-})
-
 local SafePlaceButton = Tab_Map:Button({
     Title = "TP (Safe Place)",
     Desc = "",
@@ -425,7 +411,19 @@ local SafePlaceButton = Tab_Map:Button({
         if rootPart and Baseplate then
             LastPosition = rootPart.CFrame
             rootPart.CFrame = Baseplate.CFrame + Vector3.new(0, 5, 0)
-			TpBackButton:SetDesc("last cframe: " .. tostring(LastPosition))
+        end
+    end
+})
+
+local TpBackButton = Tab_Map:Button({
+    Title = "TP Back",
+    Desc = "",
+    Locked = true,
+    Callback = function()
+        local character = game:GetService("Players").LocalPlayer.Character
+        local rootPart = character and character:FindFirstChild("HumanoidRootPart")
+        if rootPart and LastPosition then
+           rootPart.CFrame = LastPosition
         end
     end
 })
